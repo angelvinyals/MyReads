77208e73d9958929d534426119db236db606fd98
angelvinyals <angel.vinyals@gmail.com>
2017-11-22 10:10:28 +0100

    missing the imageLinks property
    create a default cover to use for books that don't have an imageLinks property. You could test using this as a default cover http://via.placeholder.com/128x193?text=No%20Cover (Use the URL in place of an image. You can customize the width, height, and text by changing the URL.)

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -23,10 +23,10 @@ class Book extends React.Component {
       authors= <div className="book-authors"><p key={this.props.book.id} className="book-authors-list">{"anonymous"}</p></div>;
     }
     let smallThumbnail
-    if (this.props.book.imageLinks.smallThumbnail){
+    if (this.props.book.imageLinks){
       smallThumbnail = this.props.book.imageLinks.smallThumbnail
     } else {
-      smallThumbnail = 'none'
+      smallThumbnail = 'http://via.placeholder.com/128x193/250/ffffff?text=No%20Cover'
     }
 
     return (
